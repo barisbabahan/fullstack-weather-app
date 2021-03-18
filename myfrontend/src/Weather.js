768ac4d53b033863606d3fd9e4b1b53c46523c8f
barisbabahan <birteko_99@hotmail.com>
2021-03-18 10:45:30 +0200

    change front end

--- myfrontend/src/Weather.js
+++ myfrontend/src/Weather.js
@@ -1,25 +1,25 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+function Weather() {
+  const [weather, setWeather] = useState("Not yet gotten!");
 
-function Weather(){
-    const [weather, setWeather] = useState("Not yet gotten!");
-
-    useEffect(()=>{
-        fetch("/weather/").then(res => {
-             if(res.ok){
-                 return res.json();
-             }
-        }).then(response => setWeather(response.weatherData))
+  const fetchWeatherTemp = async () => {
+    await axios({
+      method: "get",
+      url: "http://localhost:5000/fetchweather",
+      responseType: "stream",
+    }).then(({ data }) => {
+      setWeather(data);
     });
-        
-    
-    return(
-        <div>
-             <button onClick = {useEffect}>Get Weather in London</button>
-            <h1>The weather in London is: {weather}</h1>
-        </div>
-    )
+  };
+
+  return (
+    <div>
+      <button onClick={() => fetchWeatherTemp()}>Get Weather in London</button>
+      <h1>The weather in London is: {weather}</h1>
+    </div>
+  );
 }
 
-export default  Weather;
+export default Weather;
